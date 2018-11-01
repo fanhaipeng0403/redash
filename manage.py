541041fd5ab93a0aef6f42b5c9e146d3c22a0156
fanhaipeng <fanhaipeng0403@gmail.com>
2018-11-01 23:43:13 +0800

    update:

--- manage.py
+++ manage.py
@@ -7,3 +7,8 @@ from redash.cli import manager
 
 if __name__ == '__main__':
     manager()
+
+# docker-compose文件中有
+# command: dev_server,
+# 就是去执行entryPoint下的 dev_server函数
+# exec /app/manage.py runserver --debugger --reload -h 0.0.0.0
