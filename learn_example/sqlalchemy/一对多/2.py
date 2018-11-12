b81fb833a7d638931b667777e8be6f242578ba0b
fanhaipeng <fanhaipeng0403@gmail.com>
2018-11-12 14:34:28 +0800

    一对多“

---
+++ learn_example/sqlalchemy/一对多/2.py
@@ -0,0 +1,81 @@
+# (backref ="parent")
+"""
+https://docs.sqlalchemy.org/en/latest/orm/relationships.html
+
+    # last_modified_by = db.relationship(User, backref="modified_queries", foreign_keys=[last_modified_by_id])
+    # visualizations = db.relationship("Visualization", cascade="all, delete-orphan")
+    # user = db.relationship(User, backref='favorites')
+    # grantor = db.relationship(User, backref='grantor', foreign_keys=[grantor_id])
+    # grantor = db.relationship(User, backref='grantor', foreign_keys=[grantor_id])
+    # user = db.relationship(User, backref='changes')
+    # user = db.relationship(User, backref='changes')
+    # org = db.relationship(Organization, back_populates="events")
+
+
+主要学习RelationShip
+
+"""
+# https://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy, Model
+from sqlalchemy import Column, Integer, DateTime, func, ForeignKey, String
+
+# 2.1版本之后才可以
+from sqlalchemy.orm import relationship
+
+
+class BaseModel(Model):
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(True), default=func.now(), nullable=False)
+    updated_at = Column(DateTime(True), default=func.now(), onupdate=func.now(), nullable=False)
+
+    @classmethod
+    def create(cls, **kw):
+        session = db.session
+        if 'id' in kw:
+            obj = session.query(cls).get(kw['id'])
+            if obj:
+                return obj
+        obj = cls(**kw)
+        session.add(obj)
+        session.commit()
+        return obj
+
+    def to_dict(self):
+        columns = self.__table__.columns.keys()
+        return {key: getattr(self, key) for key in columns}
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./cnblogblog.db'
+
+db = SQLAlchemy(app, model_class=BaseModel)
+
+
+######################################################################################################
+
+class Parent(db.Model):
+    __tablename__ = 'parent'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+
+
+class Child(db.Model):
+    __tablename__ = 'child'
+    id = Column(Integer, primary_key=True)
+    parent_id = Column(Integer, ForeignKey('parent.id'))
+    name = Column(String(20))
+    parent = relationship("Parent", backref='child')  # Parent大写类名或者类名的字符串. child小写表名
+
+
+if __name__ == '__main__':
+    db.create_all()
+    Parent.create(name='ZhangTian')
+    Parent.create(name='ZhangYu')
+    Parent.create(name='LiTian')
+    Child.create(name='ZhangDi', parent_id=1)
+    Child.create(name='LiDi', parent_id=2)
+    parent = db.session.query(Child).first().parent
+    print(parent.name)
+    for child in parent.child:
+        print(child.name)
---
+++ learn_example/sqlalchemy/一对多/ReadMe.md
@@ -0,0 +1,21 @@
+# 对应关系
+一个父母多个孩子
+# 物理设计
+parent_id 放置于child表里
+
+# 1.py 
+
+` children = relationship("Child", backref ="parent") `
+relationship放置于Parent类里, Child大写类名或其字符串, 且back_ref反向引用parent(小写表名)
+
+# 2.py 
+
+` parent = relationship("Parent", backref='child') `
+relationship放置于Child类里, Parent大写 且back_ref反向引用child(小写表名)
+
+# 3.py 
+
+` children = relationship("Child", back_populates="parent") `
+` parent = relationship("Parent", back_populates='children') `
+relationship放置于于两侧，使用back_populates
+
